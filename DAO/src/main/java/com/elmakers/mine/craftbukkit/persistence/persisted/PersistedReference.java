2901f8132abfb401e3738ca4c22acda52f9fac51
Nathan Wolf <nteske@gmail.com>
2011-03-12 22:36:35 -0800

    Remove empty README

--- DAO/src/main/java/com/elmakers/mine/craftbukkit/persistence/persisted/PersistedReference.java
+++
@@ -1,9 +0,0 @@
-package com.elmakers.mine.craftbukkit.persistence.persisted;
-
-public interface PersistedReference
-{
-	public PersistedClass getReferenceType();
-	public boolean isObject();
-	public String getName();
-	public Class<?> getType();
-}
